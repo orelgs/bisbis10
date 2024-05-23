fd5637abba6765033c4f36f7e4e14a2229cc3e52
Orel Tsaraf <oreltsaraf@gmail.com>
2024-05-23 13:59:28 +0300

    Add restaurant dto with validation

---
+++ src/main/java/com/att/tdp/bisbis10/restaurant/RestaurantDTO.java
@@ -0,0 +1,30 @@
+package com.att.tdp.bisbis10.restaurant;
+
+import java.util.Set;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class RestaurantDTO {
+    @NotBlank(message = "name is required and cannot be blank")
+    private String name;
+
+    @NotNull(message = "isKosher is required")
+    private boolean isKosher;
+
+    @NotEmpty(message = "cuisines is required and must contain at least 1 cuisine")
+    private Set<@NotBlank(message = "Cuisine name cannot be blank") String> cuisines;
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isKosher() {
+        return isKosher;
+    }
+
+    public Set<String> getCuisines() {
+        return cuisines;
+    }
+}
