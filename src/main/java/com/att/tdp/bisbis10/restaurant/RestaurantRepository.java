10741dfbfb8637f383383013640040328e6e717c
Orel Tsaraf <oreltsaraf@gmail.com>
2024-05-23 10:09:28 +0300

    Add base restaurant repository

---
+++ src/main/java/com/att/tdp/bisbis10/restaurant/RestaurantRepository.java
@@ -0,0 +1,7 @@
+package com.att.tdp.bisbis10.restaurant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+}
