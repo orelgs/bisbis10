5bad2b1d6c25fe20393f8756cbbfbcaf2b4e250d
Orel Tsaraf <oreltsaraf@gmail.com>
2024-05-23 13:11:53 +0300

    Add dish controller with base functionality

---
+++ src/main/java/com/att/tdp/bisbis10/dish/DishController.java
@@ -0,0 +1,25 @@
+package com.att.tdp.bisbis10.dish;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurants/{id}/dishes")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+
+    @GetMapping
+    public ResponseEntity<List<Dish>> getAllDishesByRestaurantId(@PathVariable("id") final long restaurantId) {
+        List<Dish> dishes = dishService.getAllDishesByRestaurantId(restaurantId);
+
+        return new ResponseEntity<>(dishes, HttpStatus.OK);
+    }
+}
