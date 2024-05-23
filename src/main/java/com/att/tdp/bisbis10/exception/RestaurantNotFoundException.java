67721a3a2d4ddf339acf379c1a7804284496a6ec
Orel Tsaraf <oreltsaraf@gmail.com>
2024-05-23 12:52:20 +0300

    Create restaurant not found exception

---
+++ src/main/java/com/att/tdp/bisbis10/exception/RestaurantNotFoundException.java
@@ -0,0 +1,9 @@
+package com.att.tdp.bisbis10.exception;
+
+public class RestaurantNotFoundException extends RuntimeException {
+    private static final String errorMessage = "Restaurant with ID \"%d\" not found";
+
+    public RestaurantNotFoundException(final Long id) {
+        super(String.format(errorMessage, id));
+    }
+}
