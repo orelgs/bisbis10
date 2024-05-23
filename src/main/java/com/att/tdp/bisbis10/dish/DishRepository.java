d97e9560a33271c35739b8f507e68a14fba90330
Orel Tsaraf <oreltsaraf@gmail.com>
2024-05-23 12:51:59 +0300

    Add functionality to dish repository

--- src/main/java/com/att/tdp/bisbis10/dish/DishRepository.java
+++ src/main/java/com/att/tdp/bisbis10/dish/DishRepository.java
@@ -1,7 +1,11 @@
 package com.att.tdp.bisbis10.dish;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DishRepository extends JpaRepository<Dish, Long> {
+import com.att.tdp.bisbis10.restaurant.Restaurant;
 
+public interface DishRepository extends JpaRepository<Dish, Long> {
+    List<Dish> findAllDishesByRestaurant(final Restaurant restaurant);
 }
