2c096590ddccc6029104dd24f95e5881af9be057
Orel Tsaraf <oreltsaraf@gmail.com>
2024-05-25 23:26:12 +0300

    Change property order display on json for the restaurant projection

--- src/main/java/com/att/tdp/bisbis10/restaurant/RestaurantNoDishesProjection.java
+++ src/main/java/com/att/tdp/bisbis10/restaurant/RestaurantNoDishesProjection.java
@@ -2,6 +2,9 @@ package com.att.tdp.bisbis10.restaurant;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "name", "averageRating", "isKosher", "cuisines"})
 public interface RestaurantNoDishesProjection {
     Long getId();
     
